1b8f16ef001ecd7c44f4d625031750b2205ffb44
Nicocsanyi <beste159@yahoo.com>
2019-03-26 19:44:15 +0100

    Boot ADD

---
+++ Onepage/javascript/script.js
@@ -0,0 +1,10 @@
+easy_background("header",
+
+    {
+        slide: ["image/slider/1.jpg", "image/slider/2.jpg", "image/slider/3.jpg", "image/slider/4.jpg"],
+
+        delay: [5000, 5000, 5000, 5000]
+    }
+
+
+);
